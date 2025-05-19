4988058a3aec5bb159d1e919d064b5a5f62ee39f
subreto roy <subreto.roy@smartfieldservice.com>
2025-05-19 15:50:38 +0600

    frequently words

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import re
+from collections import Counter
+
+
+stop_words = {
+    "the", "is", "and", "a", "an", "to", "of", "in", "for", "on", "you", "can", "about", "it", "its"
+}
+
 with open("chat.txt", "r") as file:
     lines = file.readlines()
 
@@ -10,11 +18,24 @@ for line in lines:
     elif line.startswith("AI:"):
         chat["AI"].append(line.replace("AI:", "").strip())
 
-total_messages = len(chat["User"]) + len(chat["AI"])
-user_messages = len(chat["User"])
-ai_messages = len(chat["AI"])
 
-print("\n--- Message Count Summary ---")
-print(f"Total Messages: {total_messages}")
-print(f"User Messages: {user_messages}")
-print(f"AI Messages: {ai_messages}")
+# total_messages = len(chat["User"]) + len(chat["AI"])
+# user_messages = len(chat["User"])
+# ai_messages = len(chat["AI"])   
+
+# print("\n--- Message Count Summary ---")
+# print(f"Total Messages: {total_messages}")
+# print(f"User Messages: {user_messages}")
+# print(f"AI Messages: {ai_messages}")
+
+
+all_text = " ".join(chat["User"] + chat["AI"]).lower()  
+words = re.findall(r'\b\w+\b', all_text)  
+
+
+filtered_words = [word for word in words if word not in stop_words]
+word_counts = Counter(filtered_words)
+top_5 = word_counts.most_common(5)
+
+top_words = [word for word, count in top_5]
+print("\nMost common words:", ", ".join(top_words))
